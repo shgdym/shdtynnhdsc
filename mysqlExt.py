9c428b95563caf881cd7b563b36b8ae428369152
shgdym <shgdym@hotmail.com>
2020-11-28 19:19:16 +0800

    add func

--- mysqlExt.py
+++ mysqlExt.py
@@ -12,6 +12,7 @@ class MySql:
         self.password = password
         self.db = db
         self.port = port
+        self.cache = []
         self.connect()
 
     def connect(self):
@@ -39,8 +40,64 @@ class MySql:
         return row[0]
 
     def query(self, query_sql):
-        self.cursor.execute(query_sql)
-        self.dbconn.commit()
+        try:
+            self.cursor.execute(query_sql)
+            self.dbconn.commit()
+        except:
 
+            filename = 'test_text.txt'
+            with open(filename, 'a') as file_object:
+                file_object.write(query_sql)
+    def isTableExists(self, table_name):
+        if table_name in self.cache:
+            return True
+        sql = "SHOW TABLES LIKE '"+table_name+"'"
+        t = self.getFirstRow(sql)
+        if t:
+            self.cache.append(t[0])
+            return True
+        return False
+    
+    def getCreateTableSql(self, table_name):
+        sql = "SHOW CREATE TABLE `"+table_name+"`"
+        first_row = self.getFirstRow(sql)
+        return first_row[1]
+    
+    def duplicateTable(self, default_tb, new_tb):
+        create_sql = self.getCreateTableSql(default_tb)
+        search_text = "CREATE TABLE `"+default_tb+"`"
+
+        if search_text not in create_sql:
+            import sys
+            try:
+                sys.exit(0)
+            except:
+                print('die')
+        new_str = "CREATE TABLE `"+ new_tb +"`"
+        self.query(create_sql.replace(search_text, new_str, 1))
+
+				
     def close(self):
         self.cursor.close()
+
+# if __name__ == "__main__":
+#     objMysql = MySqlExt()
+#     # sql = """CREATE TABLE EMPLOYEE (
+#     #          FIRST_NAME  CHAR(20) NOT NULL,
+#     #          LAST_NAME  CHAR(20),
+#     #          AGE INT,
+#     #          SEX CHAR(1),
+#     #          INCOME FLOAT )"""
+#     # objMysql.query(sql)
+#     #
+#     # sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+#     #          LAST_NAME, AGE, SEX, INCOME)
+#     #          VALUES ('Mac', 'Mohan', 20, 'M', 2000),('john', 'Mohan', 22, 'W', 1000)"""
+#     # objMysql.query(sql)
+#
+#     sql = """SELECT age
+#              FROM EMPLOYEE
+#              where age >55"""
+#     res = objMysql.getFirstRowColumn(sql)
+#     print(res)
+#     exit()
